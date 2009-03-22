122f68a9cc31f9be68030ba0e138be424e0578db
piecho2k <piecho2k@85a6643a-1572-11de-82b5-194b17410922>
2009-03-22 03:32:48 +0000

    Agrega la configuracion para acceder a la base de datos. Aun no hay ningun caso de uso terminado.

--- src/edu/tdp2/server/SoftmartServiceImpl.java
+++ src/edu/tdp2/server/SoftmartServiceImpl.java
@@ -1,8 +1,11 @@
 package edu.tdp2.server;
 
+import org.hibernate.Session;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import edu.tdp2.client.SoftmartService;
+import edu.tdp2.server.model.HibernateUtil;
 import edu.tdp2.server.model.Usuario;
 
 public class SoftmartServiceImpl extends RemoteServiceServlet implements SoftmartService
@@ -12,7 +15,12 @@ public class SoftmartServiceImpl extends RemoteServiceServlet implements Softmar
 	@Override
 	public String login(String userName, String passwordHash)
 	{
-		new Usuario().getNombre();
+		Session sess = HibernateUtil.getSession();
+		Usuario moncho = new Usuario();
+		moncho.setNombre("Moncho");
+		sess.save(moncho);
+		sess.flush();
+		sess.close();
 		// TODO Auto-generated method stub
 		return null;
 	}
