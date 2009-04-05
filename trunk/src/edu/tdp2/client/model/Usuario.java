7842425965b8619c21955d77915520ff35475eab
piecho2k <piecho2k@85a6643a-1572-11de-82b5-194b17410922>
2009-04-05 19:56:21 +0000

    Repara algunos errores

--- trunk/src/edu/tdp2/client/model/Usuario.java
+++ trunk/src/edu/tdp2/client/model/Usuario.java
@@ -46,7 +46,7 @@ public class Usuario extends AbstractDomainObject
 	@Column(name = "PathLogo", length = 255)
 	private String pathLogo;
 
-	@OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
+	@OneToMany(mappedBy = "usuario", fetch = FetchType.EAGER)
 	private List<Proyecto> proyectos;
 
 	@OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
